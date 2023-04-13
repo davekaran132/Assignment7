ead6cabb1365ea39ed0f7413ce3efbeb13624663
davekaran132 <123052374+davekaran132@users.noreply.github.com>
2023-04-13 10:13:30 -0300

    Update challenge7.php

--- challenge7.php
+++ challenge7.php
@@ -12,17 +12,21 @@ echo '
     <body>';
     $conn = $database->connect();
 
-    $sql="SELECT *FROM STUDENTS;";
+    $sql="SELECT STUDENTS.FirstName, STUDENTS.SecondName, GRADES.Grade, COURSES.CourseName FROM STUDENTS JOIN GRADES ON GRADES.StudentId = STUDENTS.StudentId JOIN COURSES ON COURSES.CourseId = GRADES.CourseID ORDER BY COURSES.CourseName;";
     $result = $conn->query($sql);
     echo "<table>";
     echo "<tr>";
     echo "<th>First Name</th>";
     echo "<th>Last Name</th>";
+    echo "<th>Grade</th>";
+    echo "<th>Course Name</th>";
     echo "</tr>";
     while( $row = $result->fetch_assoc() ) {
         echo "<tr>";
         echo "<td>".$row['FirstName']."</td>";
         echo "<td>".$row['LastName']."</td";
+        echo "<td>".$row['Grade']."</td>";
+        echo "<td>".$row['CourseName']."</td";
         echo "</tr>";
 
     }
